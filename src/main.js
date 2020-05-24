b358ed2c0d87747eb70206740b27e6e2403d425c
Анна Колесникова <a.kolesnikova@reg.ru>
2020-05-24 16:43:41 +0300

    Правка шаблонов

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import {renderCostAndPriceTemplate} from './components/siteMain.js';
 import {renderSiteMenuTemplate} from './components/menu.js';
 import {renderFilterTemplate} from './components/filter.js';
 import {renderSortTemplate} from './components/sort.js';
+import {renderEventEditTemplate} from './components/event-edit.js';
 import {renderContentTemplate} from './components/content.js';
+import {renderPointsTemplate} from './components/points.js';
 
 renderCostAndPriceTemplate();
 renderSiteMenuTemplate();
 renderFilterTemplate();
 renderSortTemplate();
+renderEventEditTemplate();
 renderContentTemplate();
+renderPointsTemplate();
