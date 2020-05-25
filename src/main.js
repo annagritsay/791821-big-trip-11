a0c05f9b7dddac4e2c2ffe0de027bba976cc026b
Анна Колесникова <a.kolesnikova@reg.ru>
2020-05-26 00:27:32 +0300

    замена точки маршрута на форму редактирования и обратно

--- src/main.js
+++ src/main.js
@@ -12,7 +12,6 @@ const Menu = new MenuComponent(menuItems);
 const CostAndPrice = new CostAndPriceComponent();
 const Filter = new FilterComponent(filters);
 const Sort = new SortComponent();
-const EventEdit = new EventEditComponent(points[0]);
 const Content = new ContentComponent();
 
 const siteMainElement = document.querySelector(`.trip-main`);
@@ -24,17 +23,33 @@ render(tripControlsElement, Filter.getElement(), RenderPosition.BEFOREEND);
 const tripEventsElement = document.querySelector(`.trip-events`);
 render(tripEventsElement, Sort.getElement(), RenderPosition.AFTERBEGIN);
 
-const siteSortElement = document.querySelector(`.trip-sort`);
-render(siteSortElement, EventEdit.getElement(), RenderPosition.AFTERBEGIN);
-
 render(tripEventsElement, Content.getElement(), RenderPosition.BEFOREEND);
-
-const siteListElement = document.querySelector(`.trip-events__list`);
-const POINTS_COUNT = 10;
-for (let i = 0; i < POINTS_COUNT; i++) {
-  const Points = new PointsComponent(points[i]);
-  render(siteListElement, Points.getElement(), RenderPosition.BEFOREEND);
-}
 
 const siteMenuElement = document.querySelector(`.trip-controls .visually-hidden`);
 render(siteMenuElement, Menu.getElement(), RenderPosition.AFTERBEGIN);
+
+const renderPoint = (list, point) => {
+  const onEditButtonClick = () => {
+    list.replaceChild(EventEdit.getElement(), Points.getElement());
+  };
+
+  const onEditFormSubmit = (evt) => {
+    evt.preventDefault();
+    list.replaceChild(Points.getElement(), EventEdit.getElement());
+  };
+
+  const Points = new PointsComponent(point);
+  const editButton = Points.getElement().querySelector(`.event__rollup-btn`);
+  editButton.addEventListener(`click`, onEditButtonClick);
+
+  const EventEdit = new EventEditComponent(point);
+  const editForm = EventEdit.getElement().querySelector(`.event__save-btn`);
+  editForm.addEventListener(`click`, onEditFormSubmit);
+
+  render(list, Points.getElement(), RenderPosition.BEFOREEND);
+};
+const siteListElement = document.querySelector(`.trip-events__list`);
+const POINTS_COUNT = 10;
+for (let i = 0; i < POINTS_COUNT; i++) {
+  renderPoint(siteListElement, points[i]);
+}
