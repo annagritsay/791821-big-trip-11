984ec580883ac8641bdc6dd70b3b81c6f8f3420d
Анна Колесникова <a.kolesnikova@reg.ru>
2020-05-28 19:46:51 +0300

    Создаст контроллер точки маршрута

---
+++ src/controllers/point.js
@@ -0,0 +1,12 @@
+import PointsComponent from '../components/points.js';
+import EventEditComponent from '../components/event-edit.js';
+import {render, replace, RenderPosition} from "../utils/render.js";
+
+export default class PointController {
+  constructor(container) {
+  }
+
+  render(point) {
+
+  }
+}
